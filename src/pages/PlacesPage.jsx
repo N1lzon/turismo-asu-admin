a4052d3dca737324d9d2798d96fbe4bf6761347b
Nilson <nilsoncasco@outlook.com>
2026-06-21 19:18:23 -0300

    style: remove website link icon from place name column

--- src/pages/PlacesPage.jsx
+++ src/pages/PlacesPage.jsx
@@ -110,17 +110,8 @@ export default function PlacesPage() {
             <tbody>
               {filtered.map(place => (
                 <tr key={place.id}>
-                  <td className="td-name">
+                  <td>
                     <span className="place-name">{place.name}</span>
-                    {place.website && (
-                      <a href={place.website} target="_blank" rel="noopener noreferrer" className="place-web" title={place.website}>
-                        <svg width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">
-                          <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
-                          <polyline points="15 3 21 3 21 9" />
-                          <line x1="10" y1="14" x2="21" y2="3" />
-                        </svg>
-                      </a>
-                    )}
                   </td>
                   <td><CategoryBadge category={place.category} /></td>
                   <td className="td-address">
